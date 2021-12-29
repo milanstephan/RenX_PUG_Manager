8023bdf90aea85886673175a32dafac02104d7f3
Alexander Reen <alexanderreen@gmail.com>
2021-12-29 12:56:32 +0100

    add components and a gitignore

---
+++ web/src/components/Player/Player.tsx
@@ -0,0 +1,17 @@
+import "./Player.scss";
+
+interface PlayerProps {
+
+}
+
+const Player = ({}: PlayerProps) => {
+
+  const playerClass = `Player`;
+
+  return (
+    <div className={playerClass}>
+      </div>
+  );
+};
+
+export default Player;
---
+++ web/src/pages/Game/GamePage.tsx
@@ -0,0 +1,11 @@
+import './GamePage.scss'
+
+const GamePage = () => {
+
+  return (
+  <div className="Game">
+  </div>
+  );
+}
+
+export default GamePage;
